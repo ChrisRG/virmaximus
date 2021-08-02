0e23d74f58a2c54368eef3b9e1e5c781ac6afd59
Chris Geekie <chris.geekie@gmail.com>
2021-08-02 15:01:08 +0200

    Added eng, esp, fr

--- lex_parser.py
+++ lex_parser.py
@@ -4,7 +4,7 @@ import pandas as pd
 eng_lex = pd.read_excel("lex/eng_lex.xlsx", sheet_name="Feuil1", header=0)
 esp_lex = pd.read_excel("lex/esp_lex.xlsx", sheet_name="Feuil1", header=0)
 fr_lex = pd.read_excel("lex/fr_lex.xlsx", sheet_name="Feuil1", header=0)
-db_conn = sqlite3.connect("db/mydb.db")
+db_conn = sqlite3.connect("db/virmaximus.db")
 
 eng_lex.to_sql("eng_lex", db_conn, index=False, if_exists="replace")
 esp_lex.to_sql("esp_lex", db_conn, index=False, if_exists="replace")
